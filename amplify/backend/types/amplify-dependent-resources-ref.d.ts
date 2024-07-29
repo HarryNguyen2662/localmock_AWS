30f0da72bbb135c5c931c9c662bd54e92c40b7a9
Nguyen Huu Phuc Toan <toantoan123toantoan@gmail.com>
2024-07-29 17:13:58 +0700

    hello

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "slgfiles71a417f4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "predictions": {
+    "identifyText77dd4f2a": {
+      "format": "string",
+      "region": "string"
+    },
+    "interpretText99e26fde": {
+      "region": "string",
+      "type": "string"
+    }
+  },
+  "storage": {
+    "slgfiles": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
